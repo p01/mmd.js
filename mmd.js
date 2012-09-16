b3f51837265d2cc7280515eb413490c23e7b0d6b
Mathieu 'p01' Henri <mathieu@p01.org>
2012-09-16 13:20:47 +0200

    Back to checking the type of blocks by the first letter; using a hashmap {firstLetter:[regexp_split,head,lead,glue)} for the multiline blocks. Down to 817 bytes

--- mmd.js
+++ mmd.js
@@ -1,12 +1,6 @@
 ;function mmd(src)
 {
 	var h='';
-	var r=
-	[
-		/\n\* /,'<ul><li>','</li></ul>',,
-		/\n[1-9]\d*\.? /,'<ol><li>','</li></ol>',,
-		/\n    /,'<pre><code>','</pre></code>','\n',
-		/\n> /,'<blockquote>','</blockquote>','\n'];
 
 	function escape(t)
 	{
@@ -26,19 +20,24 @@
 	.replace(/^\s+|\r|\s+$/g, '')
 	.replace(/\t/g, '    ')
 	.split(/\n\n+/)
-	.forEach(function(b, f, t,R)
+	.forEach(function(b, f, R)
 	{
-		for( t='\n'+b, f=0; t.search(R=r[f]); )
-			f+=4;
-
+		f=b[0];
+		R=
+		{
+			'*':[/\n\* /,'<ul><li>','</li></ul>'],
+			'1':[/\n[1-9]\d*\.? /,'<ol><li>','</li></ol>'],
+			' ':[/\n    /,'<pre><code>','</pre></code>','\n'],
+			'>':[/\n> /,'<blockquote>','</blockquote>','\n']
+		}[f];
 		h+=
-			R?r[f+1]+t
-				.split(R)
+			R?R[1]+('\n'+b)
+				.split(R[0])
 				.slice(1)
-				.map(r[f+3]?escape:inlineEscape)
-				.join(r[f+3]||'</li>\n<li>')+r[f+2]:
-			b[0]=='#'?'<h'+(f=b.indexOf(' '))+'>'+inlineEscape(b.slice(f+1))+'</h'+f+'>':
-			b[0]=='<'?b:
+				.map(R[3]?escape:inlineEscape)
+				.join(R[3]||'</li>\n<li>')+R[2]:
+			f=='#'?'<h'+(f=b.indexOf(' '))+'>'+inlineEscape(b.slice(f+1))+'</h'+f+'>':
+			f=='<'?b:
 			'<p>'+inlineEscape(b)+'</p>';
 	});
 	return h;
