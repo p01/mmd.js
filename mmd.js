537a29db42788a94fb5c0726f03785af7b45ecb3
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-09-16 16:20:26 +0300

    s/anchor/link

--- mmd.js
+++ mmd.js
@@ -10,7 +10,7 @@
 	{
 		return escape(s)
 			.replace(/!\[([^\]]*)]\(([^(]+)\)/g, '<img alt="$1" src="$2"/>')
-			.replace(/\[([^\]]+)]\(([^(]+)\)/g, '$1'.anchor('$2'))
+			.replace(/\[([^\]]+)]\(([^(]+)\)/g, '$1'.link('$2'))
 			.replace(/`([^`]+)`/g, '<code>$1</code>')
 			.replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')
 			.replace(/\*([^*]+)\*/g, '<em>$1</em>');
